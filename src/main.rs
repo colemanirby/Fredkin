4ad972ce4f1984f19c7c0a45bf19c8dafb824d5d
colemanirby <ckirbyee92@gmail.com>
2024-06-04 16:08:29 -0500

    added summing spin at each index across all chains

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,96 @@
 use rand::Rng;
 use rand::prelude::ThreadRng;
-use std::collections::{HashSet,hash_map::DefaultHasher};
+use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
 const CHAIN_SIZE:usize = 32;
 fn main() {
 
-    let mut spin_chain_vec: Vec<SpinChain<CHAIN_SIZE>> = Vec::new();
-    let mut chain_hash_set: HashSet<u64> = HashSet::new();
-    let mut is_chain_valid: bool = false;
-    for _i in 0..20 {
-        let mut spin_chain: SpinChain<32> = SpinChain::new_empty();
+    let number_of_chains = 100;
+    let number_of_generations = 10;
+    let do_print_chains = false;
 
-        while !is_chain_valid {
-
-            spin_chain = SpinChain::new();
-
-            let mut spin_count = 0;
-            let mut size = 0;
-
-            for spin in spin_chain.chain {
-                size += 1;
-                spin_count = spin + spin_count; 
-            }
+    for _i in 1..=number_of_generations{
+        let mut spin_chain_vec: Vec<SpinChain<CHAIN_SIZE>> = Vec::new();
+        // let mut chain_hash_set: HashSet<u64> = HashSet::new();
+        let mut is_chain_valid: bool = false;
+        for _j in 0..number_of_chains {
+            let mut spin_chain: SpinChain<32> = SpinChain::new_empty();
     
-            if spin_count == 0 && !chain_hash_set.contains(&spin_chain.chain_hash){
-                is_chain_valid = true;
-                chain_hash_set.insert(spin_chain.chain_hash);
-                println!("Spin count {}", spin_count);
-                println!("Size: {}", size);
-                print!("{:?}", spin_chain.chain);
-                println!("");
-            } else {
-                let collision = chain_hash_set.contains(&spin_chain.chain_hash);
-                println!("Current Chain Hash: {}", spin_chain.chain_hash);
-                println!("Hash Set: ");
-                for hash in &chain_hash_set {
-                    print!("{}, ", hash);
+            while !is_chain_valid {
+    
+                spin_chain = SpinChain::new();
+    
+                let mut spin_count = 0;
+                let mut _size = 0;
+    
+                for spin in spin_chain.chain {
+                    _size += 1;
+                    spin_count = spin + spin_count; 
+                }
+        
+                // if spin_count == 0 && !chain_hash_set.contains(&spin_chain.chain_hash){
+                if spin_count == 0{
+                    is_chain_valid = true;
+                    // chain_hash_set.insert(spin_chain.chain_hash);
+                    // println!("Spin count {}", spin_count);
+                    // println!("Size: {}", size);
+                    // print!("{:?}", spin_chain.chain);
+                    // println!("");
+                } else {
+                    // let collision = chain_hash_set.contains(&spin_chain.chain_hash);
+                    // println!("Current Chain Hash: {}", spin_chain.chain_hash);
+                    // println!("Hash Set: ");
+                    // for hash in &chain_hash_set {
+                    //     print!("{}, ", hash);
+                    // }
+                    // println!("");
+                    // println!("Was there a collision?: {}", collision);
+                    // println!("spin count: {}", spin_count);
+                    // panic!("INVALID INVALID INVALIDDDDDD!!!!!!")
+                    println!("Invalid spin chain produced");
+                }
+                
+    
+            }
+            spin_chain_vec.push(spin_chain);
+            is_chain_valid = false;
+        }
+
+        if do_print_chains {
+            println!("SPINS");
+            for spin_chain in &spin_chain_vec {
+                for spin in spin_chain.chain {
+                    print!("{}, ",spin);
                 }
                 println!("");
-                println!("Was there a collision?: {}", collision);
-                println!("spin count: {}", spin_count);
-                // panic!("INVALID INVALID INVALIDDDDDD!!!!!!")
+                println!("____");
             }
-            
+        }
 
+        if _i == 1 {
+            println!("SPIN CHAIN CHECK: ");
         }
-        spin_chain_vec.push(spin_chain);
-        is_chain_valid = false;
-    }
     
-    println!("SPINS");
-    for spin_chain in spin_chain_vec {
-        for spin in spin_chain.chain {
-            print!("{},",spin);
-        }
-        println!("");
-        println!("____");
+        accumulate_spins_in_chain(&spin_chain_vec);
+
     }
+}
+
+/// A method that iterates through the collection of generated spin chains.
+/// It sums spins at the same site in each chain to see what the "net" spin is.
+/// Say the chain is of length 20 and this method returns that for index i there is a 20,
+/// this means that every chain generated had an up spin at this position.
+pub fn accumulate_spins_in_chain(spin_chain_vec: &Vec<SpinChain<CHAIN_SIZE>>) {
+    let mut spin_accum_array = [0;CHAIN_SIZE];
+
+    for i in 0..spin_chain_vec.len() {
+        let spin_chain = spin_chain_vec[i].chain;
+        for j in 0..spin_chain.len(){
+            spin_accum_array[j] += spin_chain[j];
+        }
+    }
+    println!("{:?}", spin_accum_array);
 }
 
 // Spin chain struct
@@ -100,7 +133,7 @@ impl<const N: usize> SpinChain<N> {
 
             let current_index: u32 = i.try_into().expect("could not make index into u32");
             let prob_up = calculate_next_spin_prob(chain_size, current_index, height);
-            println!("prob_up: {}", prob_up);
+            // println!("prob_up: {}", prob_up);
             
             let is_up_spin = determine_next_spin(prob_up, 10002);
 
@@ -111,11 +144,12 @@ impl<const N: usize> SpinChain<N> {
                 chain[i] = -1;
                 height -= 1;
             }
-            println!("Height: {}", height);
+            // println!("Height: {}", height);
         }
         chain
     }     
 }
+
 /// Calculates the probability of next spin being up. Based on Eq. 10
 /// in arXiv:1805.00532
 /// Pr(z_i+1 = up) = (h_i + 2)(N - i - h_i)/[2(h_i + 1)(N - i)]
@@ -147,8 +181,8 @@ fn determine_next_spin(prob_up: f64, trials: u128) -> bool {
         }
     }
 
-    println!("up_spin: {}", up_spin);
-    println!("down_spin: {}", down_spin);
+    // println!("up_spin: {}", up_spin);
+    // println!("down_spin: {}", down_spin);
 
     return up_spin > down_spin;
 }
