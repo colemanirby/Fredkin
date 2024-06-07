ebc3ce6c5f1e1501504d0cea6aa8134be3179331
colemanirby <ckirbyee92@gmail.com>
2024-06-07 16:34:40 -0500

    moved spin_chain struct and its generating functions to their own file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use rand::Rng;
-use rand::prelude::ThreadRng;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 use std::collections::HashMap;
+use spin_chain::SpinChain;
+mod spin_chain;
 
 const CHAIN_SIZE:usize = 32;
 fn main() {
@@ -50,85 +48,6 @@ fn main() {
         print_degen_counts(&hash_chain_map);
     }
 }
-
-
-// Spin chain struct
-pub struct SpinChain<const N: usize> {
-    pub chain: [i8;N],
-    pub chain_hash: u64,
-    pub chain_bond_rep: [char; N]
-}
-
-/// Creates a Spin Chain based on "height above horizon".
-//  Arrays:
-//    Fixed Size: Arrays have a fixed size known at compile time.
-//    Stack Allocation: Arrays are allocated on the stack, making them very fast for access and creation.
-//    No Allocation Overhead: Since the size is known at compile time, there's no need for dynamic memory allocation, which reduces overhead.
-//    Memory Access: Accessing elements in an array is very efficient because the compiler knows the size and can perform bounds checking efficiently.
-impl<const N: usize> SpinChain<N> {
-
-    pub fn new_empty() -> Self {
-        let chain = [0;N];
-        // let chain_bond_rep: [String; N] = 
-        let chain_bond_rep: [char; N] = [' '; N];
-        let chain_hash = 0;
-        SpinChain { chain, chain_hash, chain_bond_rep }
-    }
-
-    pub fn new() -> Self {
-        let mut hasher = DefaultHasher::new();
-        let chain = SpinChain::construct_chain();
-        let chain_bond_rep = SpinChain::construct_bond_rep(chain);
-        chain.hash(&mut hasher);
-        let chain_hash = hasher.finish();
-        SpinChain { chain, chain_hash, chain_bond_rep }
-    }
-
-    fn construct_chain() -> [i8;N] {
-        let mut chain = [0; N];
-        chain[0] = 1;
-        chain[N-1] = -1;
-        let chain_size: u32 = chain.len().try_into().expect("Could not turn usize into u32");
-        let mut height :u32= 1;
-        
-        // "Starting" at i = 0 with an up spin -> height starts as 1;
-        for i in 1..N - 1 {
-
-            let current_index: u32 = i.try_into().expect("could not make index into u32");
-            let prob_up = calculate_next_spin_prob(chain_size, current_index, height);
-            
-            let is_up_spin = determine_next_spin(prob_up, 10002);
-
-            if is_up_spin {
-                chain[i] = 1;
-                height += 1;
-            } else {
-                chain[i] = -1;
-                height -= 1;
-            }
-        }
-        chain
-    }
-
-    /// Temporary function that needs to be re-written
-    fn construct_bond_rep(chain: [i8;N]) -> [char;N] {
-
-        let mut chain_rep : [char;N] = [' '; N];
-
-       for i in 0..chain.len() {
-        if chain[i] == 1 {
-            chain_rep[i] = '(';
-        } else {
-            chain_rep[i] = ')';
-        }
-       }
-
-       chain_rep
-
-    }
-
-}
-
 
 /// A function to verify that a chain is well formed. That is that the net spin across the chain is zero.
 pub fn verify_chain(spin_chain: &SpinChain<CHAIN_SIZE>, hash_chain_map: &mut HashMap<u64, (u128,[i8;CHAIN_SIZE],[char;CHAIN_SIZE])>, count_degen_chains: bool) -> bool {
@@ -197,38 +116,4 @@ pub fn accumulate_spins_in_chain(spin_chain_vec: &Vec<SpinChain<CHAIN_SIZE>>) {
         }
     }
     println!("{:?}", spin_accum_array);
-}
-
-/// Calculates the probability of next spin being up. Based on Eq. 10
-/// in arXiv:1805.00532
-/// Pr(z_i+1 = up) = (h_i + 2)(N - i - h_i)/[2(h_i + 1)(N - i)]
-fn calculate_next_spin_prob(length: u32, current_index: u32, height: u32) -> f64 {
-
-    let numerator: f64 = ((height + 2) * (length - current_index - height)).try_into().expect("Could not turn u32 into f64 - numerator");
-
-    let denominator: f64 = (2 * (height + 1) * (length - current_index)).try_into().expect("Could not turn u32 into f64 - denominator");
-
-    let spin_up_prob = numerator/denominator;
-
-    spin_up_prob
-}
-
-/// Determines the next spin via taking random samples from the unit line.
-fn determine_next_spin(prob_up: f64, trials: u128) -> bool {
-
-    let mut up_spin:u128 = 0;
-    let mut down_spin:u128 = 0;
-    let mut rng: ThreadRng = rand::thread_rng();
-
-    for _i in 1..trials {
-
-        let random_num:f64 = rng.gen();
-        if random_num <= prob_up {
-            up_spin += 1;
-        } else {
-            down_spin +=1;
-        }
-    }
-
-    return up_spin > down_spin;
 }
